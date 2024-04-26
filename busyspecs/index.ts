6432a7395665c606bb4358839befea0133be9dd1
israilkhan01 <israil.khan.a888@gmail.com>
2024-04-26 13:11:40 +0530

    done initialSetUp

---
+++ busyspecs/index.ts
@@ -0,0 +1,21 @@
+import { Application } from "express";
+
+// Import the express in typescript file
+const express  = require("express");
+
+// Initialize the express engine
+const app:Application = express();
+
+// Take a port 3000 for running server.
+const port: number = 3000;
+
+// Handling '/' Request
+app.get('/', (_req, _res) => {
+	_res.send("TypeScript With Express");
+});
+
+// Server setup
+app.listen(port, () => {
+	console.log(`TypeScript with Express 
+		http://localhost:${port}/`);
+});
